cdeb86a7dcd6fdca21d8a314f19ea1cbf5c2ad26
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2021-05-07 00:58:08 +0300

    maybe all are ready

---
+++ Bootstrap-homework/site/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        'placement': 'right',
+        'template': '<div class="popover" role="tooltip"><div class="arrow"></div><span class="close btn float-right pop-close">&times;</span><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    })
+    const hideHandler = function(){
+        $('[data-toggle="popover"]').popover("hide")
+    }
+    $('[data-toggle="popover"]').on('shown.bs.popover', function () {
+        $(".close").on("click", hideHandler)
+    })
+    $('[data-toggle="popover"]').on('hidden.bs.popover', function () {
+        $(".close").off("click", hideHandler)
+    })
+})
